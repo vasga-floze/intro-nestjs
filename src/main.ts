4978d9d59cb221795461363381f2c659cdf3f3be
vasga-floze <gabriela.floze@gmail.com>
2024-01-29 00:43:30 -0600

    import the library ValidationPipe and use validations in dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // if exist a class using validator it will works, is a way to say validate all dto
   await app.listen(3000);
 }
 bootstrap();
